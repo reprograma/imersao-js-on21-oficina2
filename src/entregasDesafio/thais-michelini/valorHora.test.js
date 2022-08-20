122d0e2ba39879431139aec91adc1c9b4c95b697
thaismichelini <thaislmichelini@gmail.com>
2022-08-20 16:11:45 -0300

    test: adicionado testes para horasPorProjeto, e pacote

--- src/entregasDesafio/thais-michelini/valorHora.test.js
+++ src/entregasDesafio/thais-michelini/valorHora.test.js
@@ -2,15 +2,13 @@ const {
   calcularValorPorHora,
 } = require('../../dominio/calculadora/Hora/valorHora');
 
-test('1 + 2 é 3', () => {
-  expect(1 + 2).toBe(3);
-});
+describe('calcularValorPorHora', () => {
+  test('Calcular o valor por hora para 1000 reais mensais', () => {
+    const salario = 1000;
+    const resultadoCalculado = calcularValorPorHora(salario);
+    const resultadoEsperado = 6;
 
-test('Calcular o valor por hora para 1000 reais mensais', () => {
-  const salario = 1000;
-  const resultadoCalculado = calcularValorPorHora(salario);
-  const resultadoEsperado = 6;
-
-  expect(resultadoCalculado).toBe(resultadoCalculado);
-  expect(resultadoCalculado).not.toBe(5);
+    expect(resultadoCalculado).toBe(resultadoEsperado);
+    expect(resultadoCalculado).not.toBe(5);
+  });
 });
