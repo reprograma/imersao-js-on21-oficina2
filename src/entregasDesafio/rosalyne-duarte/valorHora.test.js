5560ea3075b687b6bfe46d336e5f923c777e7eab
Rosalyne Duarte <rosalyneduarte@gmail.com>
2022-08-20 13:18:08 -0300

    1° commit test

--- src/entregasDesafio/rosalyne-duarte/valorHora.test.js
+++ src/entregasDesafio/rosalyne-duarte/valorHora.test.js
@@ -1,17 +1,13 @@
-const { calcularValorPorHora } = require("../../dominio/calculadora/Hora/valorHora");
+const { calcularValorPorHora } = require("../../../src/dominio/calculadora/Hora/valorHora");
 
 test(' 1 + 2 é 3 ', () => {
-//  expect(1 + 2).toBe(" banana "); ERRADO
   expect(1 + 2).toBe(3);
 });
 
-test("Calcular valor por hora para salário de R$ 1.000,00 ", () => {
-    //  expect(1 + 2).toBe(" banana "); ERRADO
+test('Calcular valor por hora para salário de R$ 1.000,00 ', () => {
     const salario = 1000
     const resultadoCalculado = calcularValorPorHora(salario)
     const resultadoEsperado = 6
-
-return Math.ceil()
 
     expect(resultadoCalculado).toBe(resultadoEsperado)
     expect(resultadoCalculado).not.toBe(5)
