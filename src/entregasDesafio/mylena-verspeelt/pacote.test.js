265ddd4a2959645b1f504e57b1d9d34a88e4046d
mylenaverspeelt <mylena49@hotmail.com>
2022-08-20 17:25:51 -0300

    test: adicionar testes valorTotalProjeto e valorBaseProjeto, adicionar testes Pacotes 50/100/200

--- src/entregasDesafio/mylena-verspeelt/pacote.test.js
+++ src/entregasDesafio/mylena-verspeelt/pacote.test.js
@@ -1,24 +1,27 @@
-const {
-  calcularPacote,
-} = require("./pacote.js");
+const { calcularPacote } = require("./pacote.js");
 const horasPorProjeto = require("../../dominio/calculadora/Projeto/horasPorProjeto.js");
 jest.mock("../../dominio/calculadora/Projeto/horasPorProjeto.js");
 
 describe("calcularPacote", () => {
   test("Pacote para projeto de 64 horas", () => {
-      horasPorProjeto.calcularHorasDeProjeto.mockReturnValueOnce(64);
-
-     const horasDeProjeto = horasPorProjeto.calcularHorasDeProjeto() 
-
+    horasPorProjeto.calcularHorasDeProjeto.mockReturnValueOnce(64);
+    const horasDeProjeto = horasPorProjeto.calcularHorasDeProjeto();
     const pacote = calcularPacote(horasDeProjeto);
-
     expect(pacote).toBe("pacote_intermediario");
   });
 
-test('Pacote basico para 50 horas', () => {
-const pacote = calcularPacote(50)
+  test("Pacote basico para 50 horas", () => {
+    const pacote = calcularPacote(50);
+    expect(pacote).toBe("pacote_basico");
+  });
 
-expect(pacote).toBe('pacote_basico')
-})
+  test("Pacote basico para 100 horas", () => {
+    const pacote = calcularPacote(100);
+    expect(pacote).toBe("pacote_intermediario");
+  });
 
+  test("Pacote basico para 200 horas", () => {
+    const pacote = calcularPacote(200);
+    expect(pacote).toBe("pacote_premium");
+  });
 });
