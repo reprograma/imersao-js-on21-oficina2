619197bbd99f5316248e554ca1053020e1f66a4d
Cintia Yamamoto <cintiafumi@gmail.com>
2022-08-20 12:27:39 -0300

    test: adicionado teste para calcularValorPorHora

---
+++ src/entregasDesafio/cintia-yamamoto/valorHora.test.js
@@ -0,0 +1,14 @@
+const { calcularValorPorHora } = require('../../dominio/calculadora/Hora/valorHora')
+
+test('1 + 2 é 3', () => {
+  expect(1 + 2).toBe(3)
+})
+
+test('Calcular o valor por hora para 1000 reais mensais', () => {
+  const salario = 1000
+  const resultadoCalculado = calcularValorPorHora(salario)
+  const resultadoEsperado = 6 // 6 reais por hora
+
+  expect(resultadoCalculado).toBe(resultadoEsperado)
+  expect(resultadoCalculado).not.toBe(5)
+})
