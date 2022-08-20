646b64aebfed3cc6c50fff8ddd51048ec518b52f
Renata Rocha <renata.rocha@mjv.com.br>
2022-08-20 12:27:49 -0300

    test: adicionado teste para calcularValorHoraTRabalhada

---
+++ src/entregasDesafio/renata-valle/valorHora.test.js
@@ -0,0 +1,5 @@
+const {calcularValorPorHora} = require('../../dominio/calculadora/Hora/valorHora')
+
+test('Calcular valor por hora para salário de 1000 reais', () => {
+    expect(1 +2).toBe(3)
+})
