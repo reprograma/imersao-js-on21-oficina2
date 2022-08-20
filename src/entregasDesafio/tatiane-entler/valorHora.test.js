2fb711abfd2dfb6a9487a3a9fdd1e24ffe8606bb
Tatiane Entler <94848789+tatientler@users.noreply.github.com>
2022-08-20 15:50:46 -0300

    Novos testes

--- src/entregasDesafio/tatiane-entler/valorHora.test.js
+++ src/entregasDesafio/tatiane-entler/valorHora.test.js
@@ -1,14 +1,20 @@
 const { calcularValorPorHora } = require('../../dominio/calculadora/Hora/valorHora')
 
+/*
 test('1 + 2 é 3', () => {
     expect(1 + 2).toBe(3)
 })
-
-test('Calcular o valor por hora para R$ 1.000,00 mensais.', () => {
-    const salario = 1000
-    const resultadoCalculado = calcularValorPorHora(salario)
-    const resultadoEsperado = 6 // 6 reais por hora
-
-    expect(resultadoCalculado).toBe(resultadoEsperado)
+*/
+describe('calcularValoresMensais', () => {
+    test('Calcular o valor por hora para R$ 1.000,00 mensais.', () => {
+        const salario = 1000
+        const resultadoCalculado = calcularValorPorHora(salario)
+        const resultadoEsperado = 6 // 6 reais por hora
+    
+        expect(resultadoCalculado).toBe(resultadoEsperado)
+    })
+    
+    test('Calcular para o valor de R$ 2.000,00 mensais retornar R$ 12 por hora', () => {
+        expect(calcularValorPorHora(2000)).toBe(12)
+    })
 })
-
