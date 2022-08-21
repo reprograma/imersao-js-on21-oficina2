5b73d2676cbf66dd913872d733e835b14e3b93d4
Priscila Ferreira de Sousa <ferreiradesousap@gmail.com>
2022-08-21 17:56:27 -0300

    Adicionando melhoria no código do desafio com o uso de mock

--- src/entregasDesafio/priscila-ferreira-de-sousa/valorProjeto.test.js
+++ src/entregasDesafio/priscila-ferreira-de-sousa/valorProjeto.test.js
@@ -3,52 +3,31 @@ const horasPorProjeto = require('../../dominio/calculadora/Projeto/horasPorProje
 const valorHora  = require('../../dominio/calculadora/Hora/valorHora')
 
 jest.mock('../../dominio/calculadora/Hora/valorHora')
+jest.mock('../../dominio/calculadora/Projeto/horasPorProjeto')
 
 describe('calcularValorTotalProjeto', () => {
 
     test('Projeto de 40h com valor R$10,00 p/ hora, pacote_basico e taxa de 10% = R$440,00', () => {
-        const listaFuncionalidades = [
-            'formulario',
-            'otimizacao_seo',
-            'setup'
-        ]
-
         valorHora.calcularValorPorHora.mockReturnValueOnce(10)
         const valorPorHoraProjeto = valorHora.calcularValorPorHora()
-        const valorTotalProjeto = calcularValorTotalProjeto(listaFuncionalidades, valorPorHoraProjeto)
+        horasPorProjeto.calcularHorasDeProjeto.mockReturnValueOnce(40)
+        const valorTotalProjeto = calcularValorTotalProjeto(['teste'], valorPorHoraProjeto)
         expect(valorTotalProjeto).toBe(440)
     })
 
     test('Projeto de 80h com valor R$10,00 p/ hora, pacote_intermediario e taxa de 12% = R$960,00', () => {
-        const listaFuncionalidades = [
-            'formulario',
-            'responsividade',
-            'otimizacao_seo',
-            'integracao_mailchimp',
-            'integracao_api_propria'
-        ]
-
         valorHora.calcularValorPorHora.mockReturnValueOnce(10)
         const valorPorHoraProjeto = valorHora.calcularValorPorHora()
-        const valorTotalProjeto = calcularValorTotalProjeto(listaFuncionalidades, valorPorHoraProjeto)
+        horasPorProjeto.calcularHorasDeProjeto.mockReturnValueOnce(80)
+        const valorTotalProjeto = calcularValorTotalProjeto(['teste'], valorPorHoraProjeto)
         expect(valorTotalProjeto).toBe(960)
     })
 
     test('Projeto de 104 com valor R$10,00 p/ hora, pacote_premium e taxa de 15% = R$1560,00', () => {
-        const listaFuncionalidades = [
-            'setup',
-            'formulario',
-            'responsividade',
-            'otimizacao_seo',
-            'construcao_1_pagina',
-            'integracao_mailchimp',
-            'ssr',
-            'integracao_api_propria'
-        ]
-
         valorHora.calcularValorPorHora.mockReturnValueOnce(10)
         const valorPorHoraProjeto = valorHora.calcularValorPorHora()
-        const valorTotalProjeto = calcularValorTotalProjeto(listaFuncionalidades, valorPorHoraProjeto)
+        horasPorProjeto.calcularHorasDeProjeto.mockReturnValueOnce(104)
+        const valorTotalProjeto = calcularValorTotalProjeto(['teste'], valorPorHoraProjeto)
         expect(valorTotalProjeto).toBe(1560)
     })
 })
