fc6a7e328950638149ceead67b64c8013a78fa71
Raissa Barata <raissa.barata@ifood.com.br>
2022-08-21 00:38:01 -0300

    Finaliza implementação de testes para função valorProjeto

--- src/entregasDesafio/raissa-barata/valorProjeto.test.js
+++ src/entregasDesafio/raissa-barata/valorProjeto.test.js
@@ -1,40 +1,46 @@
-const {calcularValorTotalProjeto} = require('../../dominio/calculadora/Projeto/valorProjeto')
-
-// jest.mock('../../dominio/calculadora/Projeto/horasPorProjeto.js')
-// jest.mock('../../dominio/calculadora/Projeto/pacote.js')
+const { calcularValorTotalProjeto } = require('./valorProjeto')
 
 
-describe('valor total do projeto + 10%',() => {
-    test('teste para pacote básico', () => {
+describe('valor total do projeto + percentual de acrescimo',() => {
+
+    test('teste para pacote básico + 10%', () => {
         const valorHora = 6
         const listaDeFuncionalidades = [
             'formulario',
             'construcao_1_pagina',
             'integracao_api_propria'
         ]
+
         const resposta = calcularValorTotalProjeto(listaDeFuncionalidades, valorHora)
+
         expect(resposta).toBe(264)
     })
 
-    test('teste para pacote intermediário +  ', () => {
+    test('teste para pacote intermediário + 12%', () => {
         const valorHora = 6
         const listaDeFuncionalidades = [
             'formulario',
             'construcao_1_pagina',
-            'integracao_api_propria'
+            'integracao_api_propria',
+            'integracao_mailchimp'
         ]
         const resposta = calcularValorTotalProjeto(listaDeFuncionalidades, valorHora)
-        expect(resposta).toBe(264)
+        expect(resposta).toBe(376)
     })
 
-    test('teste para pacote básico', () => {
+    test('teste para pacote avançado + 15%', () => {
         const valorHora = 6
         const listaDeFuncionalidades = [
+            'setup',
             'formulario',
+            'responsividade',
+            'otimizacao_seo',
             'construcao_1_pagina',
+            'integracao_mailchimp',
+            'ssr',
             'integracao_api_propria'
         ]
         const resposta = calcularValorTotalProjeto(listaDeFuncionalidades, valorHora)
-        expect(resposta).toBe(264)
+        expect(resposta).toBe(718)
     })
 })
