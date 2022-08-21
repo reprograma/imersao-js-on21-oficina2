ba29e4e09aa21dbc447e39e51ce09fb5ffe21fc9
Raissa Barata <raissa.barata@ifood.com.br>
2022-08-20 23:44:20 -0300

    cria novo caso de teste e describe para funções

--- src/entregasDesafio/raissa-barata/valorHora.test.js
+++ src/entregasDesafio/raissa-barata/valorHora.test.js
@@ -1,13 +1,24 @@
 const { calcularValorPorHora } = require('../../dominio/calculadora/Hora/valorHora')
 
-test('calcular vse 1+2 é igual a  3', () => {
-    expect(1+2).toBe(3)
+describe('cria testes para funções com cenários de salários diferentes', () => {
+
+    test('calcular valor por hora para um salário de 1000', () => {
+        const salario = 1000;
+        const resultado = calcularValorPorHora(salario)
+        const resultadoEsperado = 6
+    
+        expect(resultado).toBe(resultadoEsperado)
+    })
+    
+    test('2000 reais mensais que retorna 12rs hora', () => {
+        const salario = 2000;
+        const resultado = calcularValorPorHora(salario)
+        const resultadoEsperado = 12
+    
+        expect(calcularValorPorHora(salario)).toBe(resultadoEsperado)
+    })
 })
 
-test('calcular valor por hora para um salário de 1000', () => {
-    const salario = 1000;
-    const resultado = calcularValorPorHora(salario)
-    const resultadoEsperado = 6
-
-    expect(resultado).toBe(resultadoEsperado)
+test('calcular vse 1+2 é igual a  3', () => {
+    expect(1+2).toBe(3)
 })
